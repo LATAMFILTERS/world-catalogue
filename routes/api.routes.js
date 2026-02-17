b902d2ec0da1f30f1e35c2aaf23b194a3c07d6e2
Victor Abreu <victor.abreu@elimfilters.com>
2026-02-17 00:54:24 -0600

    Eliminar referencia a pdf.routes

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -3,18 +3,12 @@ const router = express.Router();
 const donaldsonHDController = require("../controllers/donaldson.hd.controller");
 const framLDController = require("../controllers/fram.ld.controller");
 const importRoutes = require("./import.routes");
-const pdfRoutes = require("./pdf.routes");
 
 // HD scraper
 router.get("/scraper/donaldson/:code", donaldsonHDController);
-
 // LD scraper
 router.get("/scraper/fram/:code", framLDController);
-
 // Import routes
 router.use(importRoutes);
-
-// PDF processing routes
-router.use("/pdf", pdfRoutes);
 
 module.exports = router;
