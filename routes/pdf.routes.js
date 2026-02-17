d8c1a2eb16eb4003e7755b32338a5babf3b05d1b
Victor Abreu <victor.abreu@elimfilters.com>
2026-02-17 00:48:26 -0600

    Eliminar pdf-parse, pdfjs-dist y archivos relacionados

--- routes/pdf.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pdfController = require('../controllers/pdf.controller');
-
-// POST /api/pdf/process - Process a PDF file
-router.post('/process', pdfController);
-
-module.exports = router;
