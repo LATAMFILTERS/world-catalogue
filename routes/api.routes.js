c1b1d6a482788920335c30a208701262b5e7637a
Victor Abreu <victor.abreu@elimfilters.com>
2026-02-17 01:05:35 -0600

    Agregar /api/search reutilizando filterController

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const donaldsonHDController = require("../controllers/donaldson.hd.controller");
 const framLDController = require("../controllers/fram.ld.controller");
 const importRoutes = require("./import.routes");
+const { searchFilterByCode } = require("../controllers/filterController");
+
+// Search por código (lo que WordPress llama)
+router.get("/search", async (req, res) => {
+  req.params.code = req.query.q;
+  return searchFilterByCode(req, res);
+});
 
 // HD scraper
 router.get("/scraper/donaldson/:code", donaldsonHDController);
