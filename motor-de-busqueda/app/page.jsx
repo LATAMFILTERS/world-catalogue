f8a13917729f086b4dc4c092b4633ffc2d975cbd
Victor Abreu <elimfilters@gmail.com>
2026-05-04 11:06:17 -0500

    link: conectando botones VIEW ALL del home a los nuevos indices

--- motor-de-busqueda/app/page.jsx
+++ motor-de-busqueda/app/page.jsx
@@ -1,4 +1,4 @@
-'use client';
+﻿'use client';
 import Intro from './components/Intro';
 import { useState, useEffect } from 'react';
 
@@ -387,3 +387,4 @@ export default function Home() {
     </>
   );
 }
+
