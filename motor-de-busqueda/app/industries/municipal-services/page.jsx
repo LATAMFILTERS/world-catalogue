5555de66f1212263cc1c470a74f9eb366c635987
Victor Abreu <elimfilters@gmail.com>
2026-05-04 10:32:52 -0500

    feat: implementacion industria MUNICIPAL SERVICES & WASTE

---
+++ motor-de-busqueda/app/industries/municipal-services/page.jsx
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+
+export default function MunicipalServicesPage() {
+  return (
+    <div dangerouslySetInnerHTML={{ __html: 
+# 1. Preparar el componente con el HTML inyectado \$jsxContent = @" import React from 'react';  export default function MunicipalServicesPage() {   return (     <div dangerouslySetInnerHTML={{ __html: \` \$( (Get-Clipboard) -replace '\`','\\`' -replace '\\$','\\$' )     \` }} />   ); } "@  # 2. Navegar al submódulo cd "E:\Elimfilters\world-catalogue\motor-de-busqueda"  # 3. Crear el directorio de la industria \$targetPath = "app/industries/municipal-services" if (!(Test-Path \$targetPath)) { New-Item -ItemType Directory -Force -Path \$targetPath }  # 4. Guardar archivo \$jsxContent | Out-File -FilePath "\$targetPath/page.jsx" -Encoding utf8  # 5. Git Sync git add "\$targetPath/page.jsx" git commit -m "feat: implementacion industria MUNICIPAL SERVICES & WASTE" git push origin master
+     }} />
+  );
+}
