85a4f03027a4c8012cdf9e90b9bce1738ee61ddf
Victor Abreu <elimfilters@gmail.com>
2026-05-04 10:31:04 -0500

    feat: implementacion visual SERIE FH TURBINES - ET9 Separation

---
+++ motor-de-busqueda/app/technologies/fh-series/page.jsx
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+
+export default function FHSeriesPage() {
+  return (
+    <div dangerouslySetInnerHTML={{ __html: 
+# 1. Preparar el componente React con el HTML inyectado \$jsxContent = @" import React from 'react';  export default function FHSeriesPage() {   return (     <div dangerouslySetInnerHTML={{ __html: \` \$( (Get-Clipboard) -replace '\`','\\`' -replace '\\$','\\$' )     \` }} />   ); } "@  # 2. Posicionarse en el submódulo del motor de búsqueda cd "E:\Elimfilters\world-catalogue\motor-de-busqueda"  # 3. Crear el directorio de la tecnología FH \$targetPath = "app/technologies/fh-series" if (!(Test-Path \$targetPath)) { New-Item -ItemType Directory -Force -Path \$targetPath }  # 4. Generar el archivo de página \$jsxContent | Out-File -FilePath "\$targetPath/page.jsx" -Encoding utf8  # 5. Commit y Push al repositorio del submódulo git add "\$targetPath/page.jsx" git commit -m "feat: implementacion visual SERIE FH TURBINES - ET9 Separation" git push origin master
+     }} />
+  );
+}
