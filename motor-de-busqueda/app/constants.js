d1f7949d7ad88cf9cb74b8637cb0a9ed2e1fe820
LATAMFILTERS <elimfilters@gmail.com>
2026-05-10 21:54:56 -0500

    Fix: Centralize CDN to R2, update all industry pages, fix nixpacks start command

---
+++ motor-de-busqueda/app/constants.js
@@ -0,0 +1,5 @@
+// CDN URLs for media assets
+export const MEDIA_CDN = 'https://pub-fee72f3f35274550bd8a47b181823e33.r2.dev';
+
+// Legacy WordPress URL (deprecated, use MEDIA_CDN instead)
+export const WP = MEDIA_CDN;
