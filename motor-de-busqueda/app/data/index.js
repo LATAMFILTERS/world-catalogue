1373f8f527fbc5eeda2184af463b0b5595e32d22
Victor Abreu <elimfilters@gmail.com>
2026-05-15 23:24:04 -0500

    feat(data): implement unified home data layer

---
+++ motor-de-busqueda/app/data/index.js
@@ -0,0 +1,9 @@
+import industries from "./industries";
+import stats from "./stats";
+import technologies from "./technologies";
+
+export const homeData = {
+  industries,
+  stats,
+  technologies,
+};
