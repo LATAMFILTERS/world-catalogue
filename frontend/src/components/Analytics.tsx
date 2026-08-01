f4d7e3b949cee1d78b6e453464ca7ef7cb715887
LATAMFILTERS <elimfilters@gmail.com>
2026-08-01 11:48:02 -0500

    Implement GA4 Consent Mode for production detection

--- frontend/src/components/Analytics.tsx
+++ frontend/src/components/Analytics.tsx
@@ -55,8 +55,12 @@ export default function Analytics() {
   const { consent } = useConsent();
 
   const firePageView = useCallback((path: string) => {
-    if (typeof window !== 'undefined' && window.gtag && GA_ID) {
-      window.gtag('config', GA_ID, { page_path: path });
+    if (typeof window !== 'undefined' && window.gtag) {
+      window.gtag('event', 'page_view', {
+        page_path: path,
+        page_location: window.location.href,
+        page_title: document.title,
+      });
     }
     if (typeof window !== 'undefined' && window.posthog?.capture) {
       window.posthog.capture('$pageview');
@@ -67,35 +71,46 @@ export default function Analytics() {
   }, []);
 
   useEffect(() => {
-    if (consent === 'accepted') firePageView(pathname);
-  }, [pathname, firePageView, consent]);
+    if (typeof window === 'undefined' || !window.gtag) return;
 
-  if (consent !== 'accepted') return null;
+    const granted = consent === 'accepted';
+    window.gtag('consent', 'update', {
+      analytics_storage: granted ? 'granted' : 'denied',
+      ad_storage: 'denied',
+      ad_user_data: 'denied',
+      ad_personalization: 'denied',
+    });
+
+    if (granted) firePageView(pathname);
+  }, [pathname, firePageView, consent]);
 
   return (
     <>
+      <Script id="ga4-consent-default" strategy="beforeInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('consent', 'default', {
+            analytics_storage: 'denied',
+            ad_storage: 'denied',
+            ad_user_data: 'denied',
+            ad_personalization: 'denied',
+            wait_for_update: 500
+          });
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', {
+            send_page_view: false,
+            anonymize_ip: true
+          });
+        `}
+      </Script>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
         strategy="afterInteractive"
       />
-      <Script id="ga4-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_ID}', {
-            page_path: window.location.pathname,
-            send_page_view: true,
-            custom_map: {
-              dimension1: 'semantic_domain',
-              dimension2: 'concept_id',
-              dimension3: 'page_type'
-            }
-          });
-        `}
-      </Script>
 
-      {POSTHOG_KEY && (
+      {consent === 'accepted' && POSTHOG_KEY && (
         <Script id="posthog-init" strategy="afterInteractive">
           {`
             !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString()+" (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys getNextSurveyStep onSessionId setPersonPropertiesForFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
@@ -114,7 +129,7 @@ export default function Analytics() {
         </Script>
       )}
 
-      {CLARITY_ID && (
+      {consent === 'accepted' && CLARITY_ID && (
         <Script id="clarity-init" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
