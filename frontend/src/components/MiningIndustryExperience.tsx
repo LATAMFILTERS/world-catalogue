fe923d2acdd46187d317f842420a0feeb155ac37
LATAMFILTERS <elimfilters@gmail.com>
2026-08-30 21:15:08 -0500

    restore mining filter carousel

---
+++ frontend/src/components/MiningIndustryExperience.tsx
@@ -0,0 +1,11 @@
+import { MiningIndustryPageV2 } from './MiningIndustryPageV2';
+import { IndustryFilterCarousel } from './IndustryFilterCarousel';
+
+export function MiningIndustryExperience() {
+  return (
+    <>
+      <MiningIndustryPageV2 />
+      <IndustryFilterCarousel dutyClass="HD" industryName="Mining" />
+    </>
+  );
+}
